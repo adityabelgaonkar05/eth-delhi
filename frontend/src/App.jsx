4b0a39cb94b231395fedd7a24be5e652be72b6fc
Aditi Singh <aditi.singh0205@gmail.com>
2025-09-26 23:56:41 +0530

    add:tailwind config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
-import MultiplayerGame from './components/MultiplayerGame'
-import './App.css'
+import MultiplayerGame from "./components/MultiplayerGame";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <MultiplayerGame />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
